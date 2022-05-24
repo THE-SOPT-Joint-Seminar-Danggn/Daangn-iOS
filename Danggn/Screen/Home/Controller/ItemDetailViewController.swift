68c789385b805e94ae0112ad1216d72cfba47165
cchanmi <gkfn980@hs.ac.kr>
2022-05-25 01:46:17 +0900

    [Feat] Label, button 네이밍

--- Danggn/Screen/Home/Controller/ItemDetailViewController.swift
+++ Danggn/Screen/Home/Controller/ItemDetailViewController.swift
@@ -9,12 +9,27 @@ import UIKit
 
 class ItemDetailViewController: UIViewController {
 
+    @IBOutlet weak var itemPriceLabel: UILabel!
+    @IBOutlet weak var proposalPriceLabel: UILabel!
+    
+    @IBOutlet weak var chatButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setChatButton()
 
     }
 
     @IBAction func homeButtonDidTap(_ sender: UIButton) {
         self.navigationController?.popToRootViewController(animated: true)
+    }
+    @IBAction func likeButtonDidTab(_ sender: UIButton) {
+    }
+    
+    @IBAction func chatButtonDIdTab(_ sender: UIButton) {
+    }
+    
+    private func setChatButton() {
+        chatButton.layer.cornerRadius = 5
     }
 }
