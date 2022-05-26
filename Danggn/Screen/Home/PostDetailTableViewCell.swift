7e68eded41245e4b0faec31e88cc982c20b75f93
cchanmi <gkfn980@hs.ac.kr>
2022-05-27 01:06:55 +0900

    [Add] Tableview Cell 생성
    사진 tableviw cell, 게시글 tableview cell 생성

---
+++ Danggn/Screen/Home/PostDetailTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PostDetailTableViewCell.swift
+//  Danggn
+//
+//  Created by 황찬미 on 2022/05/27.
+//
+
+import UIKit
+
+class PostDetailTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Danggn/Screen/Home/PostImageTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PostImageTableViewCell.swift
+//  Danggn
+//
+//  Created by 황찬미 on 2022/05/27.
+//
+
+import UIKit
+
+class PostImageTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
