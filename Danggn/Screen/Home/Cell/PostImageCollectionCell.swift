665cee0419f959a7583f1f9c759a68c0fac634d4
cchanmi <gkfn980@hs.ac.kr>
2022-05-27 05:10:09 +0900

    [Feat] pageControl 구현
    색깔 변하는 것은 다시 구현할 예정

--- Danggn/Screen/Home/Cell/PostImageCollectionCell.swift
+++ Danggn/Screen/Home/Cell/PostImageCollectionCell.swift
@@ -10,8 +10,9 @@ import UIKit
 class PostImageCollectionCell: UICollectionViewCell {
     
     static let identifier = "PostImageCollectionCell"
-
+    
     @IBOutlet weak var postImage: UIImageView!
+    @IBOutlet weak var pageControl: UIPageControl!
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -19,6 +20,16 @@ class PostImageCollectionCell: UICollectionViewCell {
     
     func setData(postImageData: PostImageModel) {
         postImage.image = UIImage(named: postImageData.postImageName)
+    }
+
+}
+
+extension PostImageCollectionCell: UICollectionViewDelegate {
+    // feed collectionView를 스크롤하면 pageControl도 알맞은 위치로 변경
+    // 색깔이 변하지 않음...
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        let page = Int(targetContentOffset.pointee.x / self.frame.width)
+        pageControl.currentPage = page
     }
 
 }
