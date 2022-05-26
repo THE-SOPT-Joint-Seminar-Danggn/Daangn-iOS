24fa0ede772dc5c05765ff6a4cbc98386d076c40
cchanmi <gkfn980@hs.ac.kr>
2022-05-27 01:07:47 +0900

    [Add] CollectionView Cell 생성
    사진 CollectionView cell

---
+++ Danggn/Screen/Home/PostImageCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PostImageCollectionViewCell.swift
+//  Danggn
+//
+//  Created by 황찬미 on 2022/05/27.
+//
+
+import UIKit
+
+class PostImageCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
