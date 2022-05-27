607b2c584258362ae07a8cf8d2aa2b405584ca56
daminoworld <ekadls757@gmail.com>
2022-05-27 09:45:06 +0900

    [Feat] CollectionView를 이용한 아이템 리스트 구현
    - CollectionView 및 Cell xib를 이용해 아이템 리스트 구현

--- Danggn/Screen/Home/Controller/ListItemCollectionViewCell.swift
+++ Danggn/Screen/Home/Controller/ListItemCollectionViewCell.swift
@@ -8,11 +8,33 @@
 import UIKit
 
 class ListItemCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier = "ListItemCollectionViewCell"
 
+    @IBOutlet weak var listImageView: UIImageView!
+    @IBOutlet weak var listTitleLabel: UILabel!
+    @IBOutlet weak var listLocationLabel: UILabel!
+    @IBOutlet weak var listPriceLabel: UILabel!
+    @IBOutlet weak var listLikeButton: UIButton!
+    @IBOutlet weak var listLikeCount: UILabel!
+    @IBOutlet weak var listItemBorderView: UIView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
-        
+        setBorder()
     }
-
+    
+    func setData(listData: ListDataModel) {
+        listImageView.image = UIImage(named: listData.listImage)
+        listTitleLabel.text = listData.listTitle
+        listLocationLabel.text = listData.listLocation
+        listPriceLabel.text = listData.listPrice
+        listLikeCount.text = "\(listData.listLikeCount)"
+    }
+    
+    func setBorder() {
+        listItemBorderView.clipsToBounds = true
+        listItemBorderView.layer.cornerRadius = 10
+        listItemBorderView.layer.maskedCorners = CACornerMask(arrayLiteral: .layerMinXMinYCorner, .layerMaxXMinYCorner)
+    }
 }
--- Danggn/Screen/Home/Controller/ListItemViewController.swift
+++ Danggn/Screen/Home/Controller/ListItemViewController.swift
@@ -9,12 +9,21 @@ import UIKit
 
 class ListItemViewController: UIViewController {
     
+    @IBOutlet weak var listCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setNavigationUI()
+        registerCell()
     }
-    
-    private func setNavigationUI() {
+}
+
+extension ListItemViewController {
+    func setNavigationUI() {
+        
+        let navigationBar = UINavigationBarAppearance()
+        navigationBar.shadowColor = .gray
+        self.navigationController?.navigationBar.standardAppearance = navigationBar
 
         let search = navigationItem.setRightButtonUI(self, imageName: UIImage(named: "icon_search"))
         let menu = navigationItem.setRightButtonUI(self, imageName: UIImage(named: "icon_menu"))
@@ -38,9 +47,70 @@ class ListItemViewController: UIViewController {
         navigationItem.leftBarButtonItem = myArea
 
     }
-    
 }
 
 extension ListItemViewController {
+    
+    // CollectionViewCell 등록
+    func registerCell() {
+        let nib = UINib(nibName: "ListItemCollectionViewCell", bundle: nil)
+        listCollectionView.register(nib, forCellWithReuseIdentifier: "ListItemCollectionViewCell")
+        listCollectionView.delegate = self
+        listCollectionView.dataSource = self
+    }
+}
 
+extension ListItemViewController: UICollectionViewDelegate {
+    
+}
+
+extension ListItemViewController: UICollectionViewDataSource {
+    func collectionView(
+        _ collectionView: UICollectionView,
+        numberOfItemsInSection section: Int) -> Int {
+        ListDataModel.samleData.count
+    }
+    
+    func collectionView(
+        _ collectionView: UICollectionView,
+        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+            guard let cell = listCollectionView.dequeueReusableCell(
+            withReuseIdentifier: "ListItemCollectionViewCell",
+            for: indexPath) as? ListItemCollectionViewCell
+            else { return UICollectionViewCell() }
+        
+            cell.setData(listData: ListDataModel.samleData[indexPath.row])
+            return cell
+    }
+}
+
+extension ListItemViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = UIScreen.main.bounds.width
+        
+        let cellWidth = width
+        let cellHeight = width * (136/375)
+        
+        return CGSize(width: cellWidth, height: cellHeight)
+    }
+    
+    func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 20, left: 10, bottom: 10, right: 10)
+    }
+//
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+//        <#code#>
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+//        <#code#>
+//    }
 }
